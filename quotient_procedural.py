597d0c1d0d013867e74239f47f6931b782cfdd89
1526rajeev <1526rajeev@gmail.com>
2022-10-12 20:15:32 +1100

    quo_procedural

--- quotient_procedural.py
+++ quotient_procedural.py
@@ -1,5 +1,12 @@
-# QUO
-x = 30
-y = 20
-z = x // y
-print (z)
+
+##step1  get the first number
+#print("enter first input")
+x = input("enter first input")
+print("first value is " + x)
+#print("enter second input")
+y = input("enter second input")
+print("second value is" + y)
+# QUO the numbers and store the result
+print("result")
+z = (int(x)//int(y))
+print(z)
