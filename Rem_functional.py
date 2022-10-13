3d8d597b6319518320ddf9d2d8f276aceaee20fe
1526rajeev <1526rajeev@gmail.com>
2022-10-13 20:20:42 +1100

    Rem_functional

---
+++ Rem_functional.py
@@ -0,0 +1,13 @@
+def rem(x, y):
+    z = int(x)%int(y)
+    print(z)
+
+x1 = input("enter first value: ")
+if int(x1) <0:
+    print("please enter a positive value")
+    exit()
+y1 = input("enter second value: ")
+if int(y1) <0:
+    print("please enter a positive value")
+    exit()
+rem(x1,y1)
