5feae67646ed301d19bcdc9a582e9d73744fd5eb
1526rajeev <1526rajeev@gmail.com>
2022-10-20 19:34:52 +1100

    leapyear2100.py

---
+++ leapyear2100.py
@@ -0,0 +1,11 @@
+# leap year
+# i want tittle as leap year
+print("leap year")
+# give year the int input as it is a number
+year = int(input("enter year"))
+
+# check if 2100 year  is a leap year, and its divisible by 100 but not  by 400
+if(year % 100 == 0 and year % 400 != float):
+    print("yes")
+else:
+    print("no")
