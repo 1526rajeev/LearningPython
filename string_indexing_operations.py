ae040f8fd52843a131e22140d4e0eb14d81199a8
1526rajeev <1526rajeev@gmail.com>
2022-11-03 16:16:03 +1100

    string_indexing_operations.py

---
+++ string_indexing_operations.py
@@ -0,0 +1,32 @@
+#you are going to lear indexing now
+
+ # find out first character in the string using string index
+x=str(input("enter a string value"))
+print(x)
+print("you entered",x)
+print("first character in",x,"is",x[0])
+
+## find out last character in the string using string index
+x=str(input("enter a string value"))
+print(x)
+print("you entered",x)
+print("last character in",x,"is",x[-1])
+
+## find out length of the string using "len" function and print the result
+x=str(input("enter a string value"))
+print(x)
+print("you entered",x)
+print("length of the string in",x,"is",len(x))
+
+## find out the middle character in the string using  index
+
+x=str(input("enter a string value"))
+print(x)
+print("you entered",x)
+print("middle character of the string in",x,"is",len(x)//2)
+
+## find out the middle character in the string using  index (print middle character)
+x=str(input("enter a string value"))
+print(x)
+print("you entered",x)
+print("middle character of the string in",x,"is",x[len(x)//2])
